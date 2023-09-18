0bc8c347fab0c967e9ebeb5ff554e17c629b1308
Valdemar <valdemar.kobelis@gmail.com>
2023-09-18 23:41:39 +0300

    Add new actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to user_path(@user.id)
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path
+    else      
+      render :edit, status: :unprocessable_entity
     end
   end
 
